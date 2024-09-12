8891d44c5c7d68a9e1012f23c77943fc92bf6dcd
bvnovaes <bvnovaes@proton.me>
2024-09-12 12:32:08 -0300

    Alteração da versão de download do chrome.

--- PdfGenerator.Infrastructure/Adapters/PdfAdapter.cs
+++ PdfGenerator.Infrastructure/Adapters/PdfAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using PdfGenerator.Core.Domain.Interfaces;
 using PuppeteerSharp;
+using PuppeteerSharp.BrowserData;
 using PuppeteerSharp.Media;
 using System.Diagnostics.CodeAnalysis;
 
@@ -7,12 +8,31 @@ namespace PdfGenerator.Infrastructure.Adapters;
 
 [ExcludeFromCodeCoverage]
 public class PdfAdapter : IPdfGenerator
-{    
+{
     public async Task<byte[]> GeneratePdfAsync(string htmlContent)
     {
-        await new BrowserFetcher().DownloadAsync();
-        await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
-        await using var page = await browser.NewPageAsync();
+        var browserFetcher = new BrowserFetcher(new BrowserFetcherOptions
+        {
+            Browser = SupportedBrowser.ChromeHeadlessShell
+        });
+
+        InstalledBrowser revisionInfo = await browserFetcher.DownloadAsync();
+
+        var executablePath = revisionInfo.GetExecutablePath();
+
+        if (!Path.IsPathRooted(executablePath))
+        {
+            executablePath = Path.Combine(Directory.GetCurrentDirectory(), executablePath);
+        }
+
+        await using IBrowser browser = await Puppeteer.LaunchAsync(new LaunchOptions
+        {
+            Headless = true,
+            ExecutablePath = executablePath
+        });
+
+        await using IPage page = await browser.NewPageAsync();
+
         await page.SetContentAsync(htmlContent);
 
         GC.Collect();
